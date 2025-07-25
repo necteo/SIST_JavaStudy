340b0efa9b1de336c21a46b66a5b8269ea9ad55f
necteo <89801040+necteo@users.noreply.github.com>
2025-07-25 12:38:48 +0900

    a: 1주차정리/정리_5

---
+++ 1주차정리(1~3)장/src/정리_5.java
@@ -0,0 +1,41 @@
+/*
+ *   교재
+ *   4page : 실행과정
+ *   5page : 자바의 장점
+ *   14page : 바이트 코드 (.java => javac => .class)
+ *   									  ------- 바이트 코드
+ *   							 동일한 코드 => 운영체제의 호환
+ *   							 => 배포
+ *   28page : main() 설명
+ *   29page : 주석 처리 방법
+ *   30page : ;에 대한 설명
+ *   
+ *   36page : 변수의 개념
+ *   41page : 정수에 대한 설명 / 크기
+ *   43page : 정수 표현법 => 10진법 , 2진법 (0b) , 8진법(0) , 16진법(0x)
+ *   45page : 문자 => '한개문자'
+ *   47page : 실수에 대한 설명
+ *   50page : 논리 (boolean)
+ *   51page : 문자열 : String => 참조 : 52page 특수문자 참조
+ *   
+ *   55page : 자동 형변환
+ *   57page : 강제 형변환
+ *   66page : String을 각 데이터형으로 변경
+ *   72page : Scanner 설명
+ *   
+ *   ---------------------------------------------
+ *   81page : 증감연산자
+ *   83page : 산술연산자
+ *   90page : 비교연산자
+ *   93page : 논리연산자
+ *   104page : 대입연산자
+ *   105page : 삼항연산자
+ */
+public class 정리_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
