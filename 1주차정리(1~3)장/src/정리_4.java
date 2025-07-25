b72d1d71a2dc0c2eda6e9d2fe1a380b297bdfae0
necteo <89801040+necteo@users.noreply.github.com>
2025-07-25 12:23:51 +0900

    a: 1주차정리/정리_4

---
+++ 1주차정리(1~3)장/src/정리_4.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class 정리_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+//		System.out.print("세자리 정수 입력(100~999):");
+//		int num = scan.nextInt();
+//		int x = num / 100;
+//		int y = (num % 100) / 10;
+//		int z = num % 10;
+//		
+//		System.out.println("x="+x);
+//		System.out.println("y="+y);
+//		System.out.println("z="+z);
+		// 로그인 아이디 중복체크 아이디찾기 비밀번호 검색 ...
+		// 문자열 => 비교연산자 (equals)
+//		final String ID = "admin";
+//		final String PWD = "1234";
+//		
+//		System.out.print("ID를 입력:");
+//		String id = scan.next();
+//		
+//		System.out.print("비밀번호 입력:");
+//		String pwd = scan.next();
+//		
+//		String result = (id.equals(ID)) && (pwd.equals(PWD)) ? "로그인 완료" : "로그인 실패";
+//		System.out.println(result);
+		
+//		for (int i = 1; i <= 10; i+=2) {
+//			System.out.println("Hello Java!!");
+//		}
+	}
+
+}
