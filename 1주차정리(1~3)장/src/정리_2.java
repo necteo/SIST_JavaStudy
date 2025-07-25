84ded48ad181d921162c6960d17f12ce30a8b1e9
necteo <89801040+necteo@users.noreply.github.com>
2025-07-25 10:33:29 +0900

    m: 1주차정리/정리_2

--- 1주차정리(1~3)장/src/정리_2.java
+++ 1주차정리(1~3)장/src/정리_2.java
@@ -20,10 +20,10 @@
  *              | 기능 : setter / getter
  *    3) 메모리 크기 (데이터형) 
  *        정수 
- *          byte (1byte)
+ *          byte (1byte) 채팅 => byte , 파일업로드/파일 다운로드
  *          ------------------
- *          int  (4byte)
- *          long  (8byte) 
+ *          int  (4byte) = 일반 정수 => 가장 많이 사용되는 데이터형
+ *          long  (8byte) = 통계 , 확률 , 빅데이터
  *          ------------------ l(L)
  *        실수 
  *          double (8byte) => 15자리(정밀도)
@@ -31,15 +31,59 @@
  *          char (2byte) => 0~65535
  *        논리
  *          boolean (1byte) => true/false만 저장이 가능
- *    4) 변수 선언 
- *    5) 초기값 부여 
+ *    4) 변수 선언
+ *    	  데이터형 변수명; = 메모리 저장 공간 확보
+ *    			  |
+ *    			= 알파벳 , 한글로 시작
+ *    			  (대소문자 구분)
+ *    			= 숫자는 사용이 가능 (앞에 사용금지)
+ *    			= 키워드는 사용금지 (int , long...)
+ *    			= 특수문자 => (_ , $)
+ *    			= 문자길이 => 제한 (3~7)
+ *    			  ------------------- 가독성 (리팩토링)
+ *    			  => 코드리뷰 / 디자인 패턴 / 공통 모듈
+ *    						  | 8개
+ *    			  => 테스트 (JUnit)
+ *    5) 초기값 부여
+ *    	 => 변수선언 => 초기값이 없는 경우에는 사용이 불가능
+ *    		byte => 0
+ *    		int => 0
+ *    		long => 0L
+ *    		char => '\0' => ' '
+ *    		boolean => false
+ *    		double => 0.0
+ *    		String => ""
+ *    		====> 변수 => 필요시마다 변경해서 사용
+ *    
+ *    	=> 명시적 초기화  => int a=10;
+ *    	=> 사용자 입력값 : Scanner / JOptionPane ...
+ *    	=> 임의로 데이터 추출 => random
  *      
  */
 public class 정리_2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		byte b1 = Byte.MIN_VALUE;
+		byte b2 = Byte.MAX_VALUE;
+		
+		int i1 = Integer.MIN_VALUE;
+		int i2 = Integer.MAX_VALUE;
+		
+		long l1 = Long.MIN_VALUE;
+		long l2 = Long.MAX_VALUE;
+		
+		double d1 = Double.MIN_VALUE;
+		double d2 = Double.MAX_VALUE;
+		
+		char c1 = Character.MIN_VALUE;
+		char c2 = Character.MAX_VALUE;
+		
+		System.out.println("byte:"+b1+"~"+b2);
+		System.out.println("int:"+i1+"~"+i2);
+		System.out.println("long:"+l1+"~"+l2);
+		System.out.println("double:"+d1+"~"+d2);
+		System.out.println("char:"+(int)c1+"~"+(int)c2);
 	}
 
 }
