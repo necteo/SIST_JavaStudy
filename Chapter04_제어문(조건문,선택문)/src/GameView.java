47ac8b40e90c6e6e7909654109fddcd5570fa140
necteo <89801040+necteo@users.noreply.github.com>
2025-07-25 14:50:06 +0900

    m: ch4_제어문/GameView

--- Chapter04_제어문(조건문,선택문)/src/GameView.java
+++ Chapter04_제어문(조건문,선택문)/src/GameView.java
@@ -3,6 +3,9 @@ import java.awt.*; // Image
 public class GameView extends JPanel {
 	Image back, air;
 	// 초기화
+	int x = 150;
+	int y = 100;
+	
 	public GameView() {
 		// 생성자 => 변수에 대한 초기화
 		back = Toolkit.getDefaultToolkit().getImage("C:\\javaDev\\background.jpg");
@@ -11,6 +14,6 @@ public class GameView extends JPanel {
 	
 	public void paint(Graphics g) {
 		g.drawImage(back, 0, 0, getWidth(), getHeight(), this);
-		g.drawImage(air, 150, 100, 150, 100, this);
+		g.drawImage(air, x, y, 150, 100, this);
 	}
 }
--- Chapter04_제어문(조건문,선택문)/src/MainFrame.java
+++ Chapter04_제어문(조건문,선택문)/src/MainFrame.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
-//import java.awt.event.*;
-public class MainFrame extends JFrame {
+import java.awt.event.*;
+public class MainFrame extends JFrame implements KeyListener {
 	GameView gv = new GameView();
 	
 	public MainFrame() {
@@ -9,11 +9,47 @@ public class MainFrame extends JFrame {
 		setSize(800, 600);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		addKeyListener(this);
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		new MainFrame();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getSource() == this) { // 윈도우에서 키보드 처리
+			if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+				gv.x -= 5;
+				if (gv.x < 0)
+					gv.x = getWidth();
+				gv.repaint();
+			}
+			if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+				gv.x += 5;
+				if (gv.x > getWidth())
+					gv.x = 0;
+				gv.repaint();
+			}
+			if (e.getKeyCode() == KeyEvent.VK_UP) {
+				gv.y -= 5;
+				if (gv.y < 0)
+					gv.y = getHeight();
+				gv.repaint();
+			}
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
