471b817c2b5365fabb35f574dbe37fa5cbc3d6c6
necteo <89801040+necteo@users.noreply.github.com>
2025-07-28 17:52:09 +0900

    a: ch4_제어문문제/문제정리

---
+++ Chapter04_제어문(조건문,선택문문제)/src/문제정리.java
@@ -0,0 +1,38 @@
+/*
+ *    1. 형식
+ *    	 단일 조건문 => 113page
+ *    	 if(조건문) => 검색 , 로그인 , 아이디 찾기
+ *    				  비밀번호 찾기 , 아이디 중복체크
+ *    				  아이디저장 , 자동 로그인
+ *    		----- 부정연산자 , 비교연산자 , 논리연산자
+ *    		----- equals(같다) , contains(포함)
+ *    		----- startsWith(시작문자) , endsWith(끝문자열)
+ *    		----- !equals()
+ *    	 {
+ *    		실행문장  => 조건문이 true일 경우에만 실행
+ *    				   false면 건너뛴다
+ *    	 }
+ *    
+ *    	 if~else => 114page
+ *    	 true / false => 웹에서는 반드시 성공/실패 => 메세지 전송
+ *    	 
+ *    	 if(조건문)
+ *    	 {
+ *    		로그인 성공 : 조건문 true
+ *    	 }
+ *    	 else
+ *    	 {
+ *    		로그인 실패 : 조건문 false
+ *    	 }
+ *    	 ------------------------------------------------
+ *    	 다중 조건문 => 115page
+ *    	 
+ */
+public class 문제정리 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
